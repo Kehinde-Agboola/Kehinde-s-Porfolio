346c62406d15911d2703a82305fb13ffd6e100a2
Your Name <kehindeagboola52@gmail.com>
2022-10-07 10:33:25 +0100

    chnage the contact from

--- js/init.js
+++ js/init.js
@@ -131,6 +131,10 @@ jQuery(document).ready(function ($) {
   /*	contact form
 ------------------------------------------------------*/
 
+  function resetForm() {
+    $("#form")[0].reset();
+  }
+
   $("form #contactForm button.submit").click(function () {
     $("#image-loader").fadeIn();
 
@@ -170,5 +174,55 @@ jQuery(document).ready(function ($) {
       },
     });
     return false;
+    resetForm();
   });
 });
+
+// action="https://formspree.io/f/xknebpaa"
+
+const nameEl = document.querySelector("#name");
+const emailEl = document.querySelector("#email");
+const companyNameEl = document.querySelector("#company-name");
+const messageEl = document.querySelector("#message");
+
+// const form = document.querySelector("#submit-form");
+
+// function checkValidations() {
+//   let letters = /^[a-zA-Z\s]*$/;
+//   const name = nameEl.value.trim();
+//   const email = emailEl.value.trim();
+//   const companyName = companyNameEl.value.trim();
+//   const message = messageEl.value.trim();
+//   if (name === "") {
+//     document.querySelector(".name-error").classList.add("error");
+//     document.querySelector(".name-error").innerText =
+//       "Please fill out this field!";
+//   } else {
+//     if (!letters.test(name)) {
+//       document.querySelector(".name-error").classList.add("error");
+//       document.querySelector(".name-error").innerText =
+//         "Please enter only characters!";
+//     } else {
+//     }
+//   }
+//   if (email === "") {
+//     document.querySelector(".name-error").classList.add("error");
+//     document.querySelector(".name-error").innerText =
+//       "Please fill out this field!";
+//   } else {
+//     if (!letters.test(name)) {
+//       document.querySelector(".name-error").classList.add("error");
+//       document.querySelector(".name-error").innerText =
+//         "Please enter only characters!";
+//     } else {
+//     }
+//   }
+// }
+
+function reset() {
+  nameEl = "";
+  emailEl = "";
+  companyNameEl = "";
+  messageEl = "";
+  document.querySelector(".name-error").innerText = "";
+}
